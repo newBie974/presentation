1b1e92871c9ac678f53ed8f8080b86ed9ed7ae02
Aymeric Dijoux <aymeric@Aymerics-MacBook-Pro.local>
2026-01-24 18:56:49 +0100

    feat(githubIcon): add github icon on list

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -2,16 +2,6 @@ import type { SocialLink } from "../types/index";
 
 export const socialLinks: SocialLink[] = [
   {
-    platform: "linkedin",
-    url: "https://linkedin.com/in/aymeric",
-    label: "Follow on LinkedIn",
-  },
-  {
-    platform: "x",
-    url: "https://x.com/aymeric",
-    label: "Follow on X",
-  },
-  {
     platform: "instagram",
     url: "https://instagram.com/aymeric",
     label: "Follow on Instagram",
@@ -20,5 +10,20 @@ export const socialLinks: SocialLink[] = [
     platform: "tiktok",
     url: "https://tiktok.com/@aymeric",
     label: "Follow on TikTok",
+  },
+  {
+    platform: "linkedin",
+    url: "https://www.linkedin.com/in/aymeric-dijoux",
+    label: "Follow on LinkedIn",
+  },
+  {
+    platform: "x",
+    url: "https://x.com/AymericDijoux",
+    label: "Follow on X",
+  },
+  {
+    platform: "github",
+    url: "https://github.com/newBie974",
+    label: "Follow on GitHub",
   },
 ];
