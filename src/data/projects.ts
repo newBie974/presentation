447abc4886dfdc151574bcb72e780f44843344dc
Aymeric Dijoux <aymeric@Aymerics-MacBook-Pro.local>
2026-01-24 11:09:40 +0100

    chore(projects): update project release

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,11 +2,18 @@ import type { Project } from "../types/index";
 
 export const projects: Project[] = [
   {
+    title: "Carrousel.ai",
+    tagline: "Ai helps you to create the best carroussel for Tiktok, Instagram",
+    url: "#",
+    status: "coming-soon",
+    techStack: ["", "", ""],
+  },
+  {
     title: "Tookta",
     tagline: "Find the perfect activity for your kids, effortlessly",
     url: "https://tookta.fr",
-    status: "building",
-    techStack: ["Flutter", "TypeScript", "NestJs"],
+    status: "live",
+    techStack: ["Flutter", "Nest", "TypeScript", "Typesense"],
     appStoreUrl: "https://apps.apple.com/fr/app/tookta/id6474099484",
     playStoreUrl: "https://play.google.com/store/apps/details?id=com.tookta.fr",
   },
